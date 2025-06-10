8e7b3fcc9ae486b39b9faf6e75b62c04f166e54e
Robert Burns <122560076+robertdburns@users.noreply.github.com>
2025-06-09 20:08:09 -0700

    Update conway_tb.sv

--- tests/conway_tb/conway_tb.sv
+++ tests/conway_tb/conway_tb.sv
@@ -6,19 +6,23 @@ module conway_tb;
     logic clk;
     logic rst;
 
-    logic state;		// 0 = load, 1 = run
-    logic [15:0] addr;
+    logic state;        // 0 = load, 1 = run
+    logic [8:0] addr;
+    logic [7:0] data;    // data input for loading the byte at addr
 
-	logic next_gen;
+    logic next_gen;
+    logic INIT_DONE;
 
     // Unit Under Test
-    conway #(
+    conway_new #(
     ) uut (
         .clk(clk),
         .rst(rst),
-        .state(state),
+        .run(state),
         .addr(addr),
-		.next_gen(next_gen)
+        .data(data),
+        .next_gen(next_gen),
+        .INIT_DONE(INIT_DONE)
     );
 
     // Clock generation
@@ -27,7 +31,7 @@ module conway_tb;
         forever #5 clk = ~clk; // 100 MHz clock
     end
 
-	initial begin
+    initial begin
     // Name as needed
     $dumpfile("tb_conway.vcd");
     $dumpvars(0);
@@ -38,40 +42,44 @@ end
         // Initialize signals
         rst = 1;
         state = 0;
-		
+
 
         // Reset the UUT
         #10 rst = 0;
 
         // Add stimulus here
         // simple glider design found on google
-		addr = 16'h02ff; // Load address
-		#10;
-		addr = 16'h01fd; // Load address
-		#10;
-		addr = 16'h02fd; // Load address
-		#10;
-		addr = 16'h03fd; // Load address
-		#10;
-		addr = 16'h03fe; // Load address
-		#10;
+        wait(INIT_DONE == 1); // Wait for initialization to complete
+        #10;
+        data = 'd255;
+        addr = {7'b0000000, 2'b11}; // Load address
+        #10;
+        data = 'd255;
+        addr = {7'b0000001, 2'b11}; // Load address
+        #10;
+        data = 'd255;
+        addr = {7'b0000010, 2'b11}; // Load address
+        #10;
+        data = 'd255;
+        addr = {7'b0000011, 2'b11}; // Load address
+        #10;
         state = 1; // Start running
-		#20
+        #20
 
-		// for (logic [7:0] i = 0; i <= 8'd0; i++) begin
-		// 	wait(next_gen == 1);		// generation 1
-		// 	$display("Generation %0d", i);
-		//  	#20;
-		// end
+        // for (logic [7:0] i = 0; i <= 8'd0; i++) begin
+        //     wait(next_gen == 1);        // generation 1
+        //     $display("Generation %0d", i);
+        //      #20;
+        // end
 
-		 wait(next_gen == 1);		// generation 1
-		 #20;
-		 wait(next_gen == 1); 		// generation 2
-		 #20;
-		//  wait(next_gen == 1);		// generation 3
-		//  #20;
-		//  wait(next_gen == 1);		// generation 4
-		//  #20;
+         wait(next_gen == 1);        // generation 1
+         #20;
+         wait(next_gen == 1);         // generation 2
+         #20;
+         wait(next_gen == 1);        // generation 3
+         #20;
+         wait(next_gen == 1);        // generation 4
+         #20;
         $finish;
     end
 
